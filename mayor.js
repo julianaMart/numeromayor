49cdd16de96b8a980978811c24ceaa57a1b71164
Juliana Martinez Gomez <Juliana.martinez@sinco.com.co>
2023-03-17 21:51:54 -0500

    Ejercicio numero menor y mayor

---
+++ mayor.js
@@ -0,0 +1,27 @@
+let a=100;
+let b=50;
+
+if (a<b)
+{
+    console.log(a+" es el número menor");
+}
+else{
+    console.log(b+" es el número menor");
+}
+
+
+
+let j=35;
+let k=20;
+let l=10;
+
+if ((j>k) && (j>l)){
+        console.log(j+" es el numero mayor");
+        if ((k>j) && (k>l)){
+            console.log(k+" es el numero mayor");
+        }
+}
+else{
+
+    console.log(l+" es el numero mayor");
+}
